01f8f4a8a5b36013b0abf8bccbd44e43d0409765
Armen Mirzoyan Denisov <100298950+armirzoya@users.noreply.github.com>
2022-06-06 10:59:36 +0200

    Initial commit

---
+++ src/main/java/com/ccsw/bidoffice/BidofficeServerApplication.java
@@ -0,0 +1,13 @@
+package com.ccsw.bidoffice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BidofficeServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BidofficeServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ccsw/bidoffice/BidofficeServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ccsw.bidoffice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BidofficeServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
