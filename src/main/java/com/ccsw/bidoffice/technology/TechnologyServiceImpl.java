350864296d8512008bd23634a6a5deede32be0b0
ESDLH <72152725+ESDLH@users.noreply.github.com>
2022-08-04 09:23:41 +0200

    BD-12-Listado-de-tecnologias

--- src/main/java/com/ccsw/bidoffice/technology/TechnologyServiceImpl.java
+++ src/main/java/com/ccsw/bidoffice/technology/TechnologyServiceImpl.java
@@ -1,8 +1,23 @@
 package com.ccsw.bidoffice.technology;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+
+import com.ccsw.bidoffice.technology.model.TechnologyEntity;
 
 @Service
 public class TechnologyServiceImpl implements TechnologyService {
 
+    @Autowired
+    TechnologyRepository technologyRepository;
+
+    @Override
+    public List<TechnologyEntity> findAllOrderByPriority() {
+
+        return (List<TechnologyEntity>) this.technologyRepository.findAll(Sort.by(Sort.Direction.ASC, "priority"));
+
+    }
 }
