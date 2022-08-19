0707c1ccd16fc0e1c5a8b724b2dfeb037ed5708b
ESDLH <72152725+ESDLH@users.noreply.github.com>
2022-08-19 10:58:31 +0200

    Modelo y Entity de Sector.

--- src/main/java/com/ccsw/bidoffice/sector/model/SectorDto.java
+++ src/main/java/com/ccsw/bidoffice/sector/model/SectorDto.java
@@ -1,5 +1,7 @@
 package com.ccsw.bidoffice.sector.model;
 
+import java.time.LocalDate;
+
 public class SectorDto {
 
     private Long id;
@@ -7,6 +9,10 @@ public class SectorDto {
     private String name;
 
     private Integer priority;
+
+    private LocalDate start_date;
+
+    private LocalDate end_date;
 
     public Long getId() {
         return id;
@@ -31,4 +37,21 @@ public class SectorDto {
     public void setPriority(Integer priority) {
         this.priority = priority;
     }
+
+    public LocalDate getStart_date() {
+        return start_date;
+    }
+
+    public void setStart_date(LocalDate start_date) {
+        this.start_date = start_date;
+    }
+
+    public LocalDate getEnd_date() {
+        return end_date;
+    }
+
+    public void setEnd_date(LocalDate end_date) {
+        this.end_date = end_date;
+    }
+
 }
--- src/main/java/com/ccsw/bidoffice/sector/model/SectorEntity.java
+++ src/main/java/com/ccsw/bidoffice/sector/model/SectorEntity.java
@@ -1,5 +1,7 @@
 package com.ccsw.bidoffice.sector.model;
 
+import java.time.LocalDate;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -21,6 +23,12 @@ public class SectorEntity {
 
     @Column(name = "priority", nullable = false)
     private Integer priority;
+
+    @Column(name = "start_date")
+    private LocalDate start_date;
+
+    @Column(name = "end_date")
+    private LocalDate end_date;
 
     public Long getId() {
         return id;
@@ -44,5 +52,21 @@ public class SectorEntity {
 
     public void setPriority(Integer priority) {
         this.priority = priority;
+    }
+
+    public LocalDate getStart_date() {
+        return start_date;
+    }
+
+    public void setStart_date(LocalDate start_date) {
+        this.start_date = start_date;
+    }
+
+    public LocalDate getEnd_date() {
+        return end_date;
+    }
+
+    public void setEnd_date(LocalDate end_date) {
+        this.end_date = end_date;
     }
 }
