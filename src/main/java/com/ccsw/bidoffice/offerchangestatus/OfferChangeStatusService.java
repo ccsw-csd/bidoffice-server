3ca53981c053dc89eb08853a1d975fde6535bf7c
Candela101 <cabero01@floridauniversitaria.es>
2023-05-22 09:39:38 +0200

    añado los cambios para poder ver el historial de cambios de estado

--- src/main/java/com/ccsw/bidoffice/offerchangestatus/OfferChangeStatusService.java
+++ src/main/java/com/ccsw/bidoffice/offerchangestatus/OfferChangeStatusService.java
@@ -2,11 +2,9 @@ package com.ccsw.bidoffice.offerchangestatus;
 
 import java.util.List;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.ccsw.bidoffice.offerchangestatus.model.OfferChangeStatusEntity;
 
-public interface OfferChangeStatusRepository extends JpaRepository<OfferChangeStatusEntity, Long>{
+public interface OfferChangeStatusService {
 
     List<OfferChangeStatusEntity> findByOfferId(Long id);
 }
